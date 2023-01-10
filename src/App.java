a1ad371938c53c2e0be4e1fff54f75a69fc428ee
Luís Henrique <luishsilva09@gmail.com>
2023-01-10 15:16:13 -0300

    exercicio separado por classe

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args)  {
+        System.out.println("MultiplicaAí");
+
+        MutiplicaAi multiplicacao = new MutiplicaAi();//crinado intancia da classe
+         
+        multiplicacao.mutiplicaAi(2);// Escolher numero de qual vai ser a multiplicação
+    }
+}
---
+++ src/MutiplicaAi.java
@@ -0,0 +1,11 @@
+public class MutiplicaAi {
+
+    //criando o metodo da classe
+    public static void mutiplicaAi(int numero){
+        System.out.println("Tabela mutiplição de " + numero);
+
+        for(int i = 1; i < 11; i++){
+            int res = i * numero;
+            System.out.println(numero+ " X " + i + " = " + res);
+        }}
+}
